9a662c556519974c6167df16a7a9a07902ebc0cf
MrGoopyDrawers <107856947+MrGoopyDrawers@users.noreply.github.com>
2023-08-10 19:01:37 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "RaiseAMonkey";
 
-    public const string Description = "Raise a monkey and train it ";
+    public const string Description = "Raise a monkey and train it to become better p.s to restart the mod, you have to create a whole new game, restart button is bugged for some reason and i aint finding a goofy ah harmony patch for it";
 
     public const string RepoOwner = "MrGoopyDrawers"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "RaiseAMonkey"; // TODO add your repo name here, also in the download url in README.md
